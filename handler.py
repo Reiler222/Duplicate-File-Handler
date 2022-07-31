2e76f0ebcf682fe4f2db4358f9db67c9311d8532
Andrés Urraca <andres.urraca@autentia.com>
2022-07-31 19:52:19 +0200

    Add some useless experiments so far

--- handler.py
+++ handler.py
@@ -14,9 +14,9 @@ def output(dict_to_output):
 
 def sorting():
     if sorting_option == '1':
-        sorted_dict = {k: [v] for k, v in sorted(inventory_dict.items(), key=lambda item: item[0], reverse=True)}
+        sorted_dict = {k: v for k, v in sorted(inventory_dict.items(), key=lambda item: item[0], reverse=True)}
     else:
-        sorted_dict = {k: [v] for k, v in sorted(inventory_dict.items(), key=lambda item: item[0])}
+        sorted_dict = {k: v for k, v in sorted(inventory_dict.items(), key=lambda item: item[0])}
     print()
     output(sorted_dict)
     print(sorted_dict)
@@ -29,7 +29,10 @@ def find_files():
             if name.endswith(format):
                 complete_name = os.path.join(root, name)
                 size = os.path.getsize(complete_name)
-                inventory_dict[size] = complete_name
+                if size in inventory_dict.keys():
+                    inventory_dict[size].append(complete_name)
+                else:
+                    inventory_dict[size] = complete_name
     sorting()
 
 try:
@@ -48,3 +51,21 @@ while True:
         break
 
 find_files()
+try:
+    root = args[1]
+    extension = input('Enter file format:\n')
+    for root, dirs, files in os.walk(".", topdown=True):
+        for name in files:
+            filename, file_extension = os.path.splitext(name)
+            if file_extension == extension or extension == '':
+                file_path = os.path.join(root, name)
+                size = os.path.getsize(file_path)
+                if size in size_paths_dic.keys():
+                    size_paths_dic[size].append(file_path)
+                else:
+                    paths_list = [file_path]
+                    size_paths_dic[size] = paths_list
+                #print(os.path.join(root, name))
+    sorting_opt()
+except IndexError:
+    print('Directory is not specified')
